98006f11a9f771ed146e02dba3cb68b3b82eba6d
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-09-16 18:11:31 +0200

    Feat. add commit in code

--- Shortest Word/js/script.js
+++ Shortest Word/js/script.js
@@ -2,8 +2,8 @@ const string1 = "Simple, given a string of words, return the length of the short
 const string2 = "turns out random test cases are easier than writing out basic ones";
 const string3 = "Let's travel abroad shall we";
 
-const arrayString = [];
-arrayString.push(string1,string2,string3);
+const arrayString = [string1, string2, string3];
+
 
 // for(let i = 0; i < arrayString.length; i ++) {
 //    let min = arrayString[0];
@@ -15,11 +15,20 @@ arrayString.push(string1,string2,string3);
 //    }
 // }
 
+// Siegazione esercizio
+/*
+  dato un array di stringhe devo trovare la stringa più corta 
+  salvo in una variabile il primo elemento dell array con indice 0
+  poi nella condizione dico che l elemento corrente è minore del primo elemento
+  il ciclo itera finchè questa condizione non diventa vera, trovando l elemento 
+  più piccolo dell array che salverò a questo punto nella variabile min
+*/ 
+
 function shortString(array) {
     for(let i = 0; i < array.length; i++) {
-        let min = array[0];
-        if(array[i] < min) {
-            return min = array[i];
+        let min = array[0];        // prendo come riferimento il primo elemento del array
+        if(array[i] < min) {       // se l elemento iesimo è minore di min 
+            return min = array[i]; // in min salva l elemento più piccolo
         }
     }
 }
