3745b27a459fbdb243b62eaabb29d947c723ad5d
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-09-16 18:05:37 +0200

    Feat: Added function shortString

--- Shortest Word/js/script.js
+++ Shortest Word/js/script.js
@@ -5,12 +5,25 @@ const string3 = "Let's travel abroad shall we";
 const arrayString = [];
 arrayString.push(string1,string2,string3);
 
-for(let i = 0; i < arrayString.length; i ++) {
-   let min = arrayString[0];
-   let minElem = arrayString[i] < min;
-   console.log(minElem);
-   if(arrayString[i] < min) {
-     min = arrayString[i]
-     console.log(min)
-   }
+// for(let i = 0; i < arrayString.length; i ++) {
+//    let min = arrayString[0];
+//    let minElem = arrayString[i] < min;
+//    console.log(minElem);
+//    if(arrayString[i] < min) {
+//      min = arrayString[i]
+//      console.log(min)
+//    }
+// }
+
+function shortString(array) {
+    for(let i = 0; i < array.length; i++) {
+        let min = array[0];
+        if(array[i] < min) {
+            return min = array[i];
+        }
+    }
 }
+
+const result = shortString(arrayString);
+
+console.log("Stringa più corta:", result)
